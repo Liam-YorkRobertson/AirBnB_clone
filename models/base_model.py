ff2b0f446e4c053a5c03cbb012143fa7ea274404
Mathewos K <mathewos1@yahoo.com>
2023-07-13 10:03:58 -0700

    base model and storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """
 Defines class BaseModel.
 """
+import models
 import uuid
 from datetime import datetime
 
@@ -11,13 +12,25 @@ class BaseModel:
     Class BaseModel defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()  # updated when initialized
+        if kwargs:
+            for key, val in kwargs.items():
+                """
+                convert date and time keys to obj
+                """
+                tFormat = "%Y-%m-%dT%H:%M:%S.%f"
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, tFormat)
+                else:
+                    self.__dict__[key] = val
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -30,6 +43,7 @@ class BaseModel:
         Updates updated_at.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+file storage for creating writing and reading json for storage
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    for file storage
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """
+        returns the dictionary __objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        file_id = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[file_id] = obj
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        fileName = FileStorage.__file_path
+        fileObj = FileStorage.__objects
+        storeD = {obj: fileObj[obj].to_dict() for obj in fileObj.keys()}
+        with open(fileName, "w") as fN:
+            json.dump(storeD, fN)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects (only if the JSON file exists;
+        otherwise, do nothing. If the file doesn’t exist, no exception  raised)
+        """
+        fileName = FileStorage.__file_path
+        FileStorage.__objects = {}
+        try:
+            with open(fileName) as StrJ:
+                storeD = json.load(StrJ)
+        except FileNotFoundError:
+            return
+        for value in storeD.values():
+            clsName = value["__class__"]
+            del value["__class__"]
+            self.new(eval(clsName)(**value))
