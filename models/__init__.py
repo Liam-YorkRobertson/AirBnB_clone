97cbe5126d500cfbcd10e26152c8a49556b9e315
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2023-07-16 09:47:45 -0700

    Made changes to /models/__init__.py, /models/engine/__init__.py and console.py to resolve errors in console.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,10 @@
 initialize magic method for models directory
 """
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
-
+classes = {
+    'BaseModel': BaseModel
+}
 storage = FileStorage()
 storage.reload()
---
+++ models/engine/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+Engine package for the models module.
+"""
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
