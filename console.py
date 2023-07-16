b2e9022c98248475630b229b815463dfff53ba76
Mathewos K <mathewos1@yahoo.com>
2023-07-16 10:53:20 -0700

    rm user from console

--- console.py
+++ console.py
@@ -10,12 +10,6 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
 from models import *
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
