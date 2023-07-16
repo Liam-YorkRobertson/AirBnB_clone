959480b78440623c1a86d1fe55d151e61d0fb0a0
Mathewos K <mathewos1@yahoo.com>
2023-07-16 07:40:04 -0700

    filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,12 +14,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """
         returns the dictionary __objects
         """
-        FileObj = FileStorage.__objects
-        return Fileobj
+        return FileStorage.__objects
 
     def new(self, obj):
         """
@@ -54,16 +53,3 @@ class FileStorage:
             clsName = value["__class__"]
             del value["__class__"]
             self.new(eval(clsName)(**value))
-
-    def delete(self, obj=None):
-        """Deletes an object if it exists."""
-        FileId = "{}.{}".format(type(obj).__name__, obj.id)
-        FileObj = FileStorge._objects
-        try:
-            del FileObj[FileId]
-        except (AttributeError, KeyError):
-            pass
-
-    def close(self):
-        """Call the reload function"""
-        FileStorage.reload()
